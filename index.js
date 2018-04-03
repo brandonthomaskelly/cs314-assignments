b35c33e5b57bc495922db7e4e17ce66be1eb761c
Brandon Kelly <brandonthomaskelly@gmail.com>
2018-04-03 14:19:18 -0700

    Initial Setup
    Removal of reset+style and added index.js with notes on assignment as well as index.html setup.

---
+++ index.js
@@ -0,0 +1,55 @@
+// Brandon Kelly
+
+// 1. Char Swap - Write a JavaScript function to create a new string from an input string from
+// the user swapping the position of first and last characters. The string length entered by
+// the user must be greater than or equal to 1.
+
+
+// 2. Hi String - Write a JavaScript function that asks for a string from the user and returns a
+// new string adding "Hi" in front of the input string. If the input string begins with "Hi" then
+// return the original string. The string length entered by the user must be greater than or
+// equal to 1.
+
+
+// 3. Three Chars to Front - Write a JavaScript function to create a new string from an input
+// string taking the last 3 characters and adding them to the front of the string. The string
+// length entered by the user must be at least 3 characters long.
+
+
+// 4. Strings to Sentence - Write a JavaScript function that asks for a list of items from the
+// user separated by commas. Convert this to an array without the commas or extra
+// spaces. Alert the items back to the user with a single saying that is formed using
+// backticks (``).
+// Example:
+// input from user: blue, dogs, chocolate
+// output from function: My favorite color is blue, dogs are awesome, and I love chocolate!
+
+
+// 5. Upper or Lower - Write a JavaScript function that asks for a string from the user and
+// creates a new string from that string where the first 3 characters are lowercase. If the
+// string length entered by the user is less than 3 convert all the characters in upper case.
+
+
+// 6. Integer Swap - Write a JavaScript function that asks for a comma separated list of
+// numbers from the user and swap the first and last elements of a given array of integers.
+// Alert the result to the user. The array length given from the user should be at least 1.
+
+
+// 7. Longest String - Write a JavaScript function that asks for a comma separated list of
+// strings from the user and alerts the longest string from the given array of strings.
+
+
+// 8. Largest Even Number - Write a JavaScript function that asks for a comma separated list
+// of numbers from the user. Convert this list to an array of numbers and alert the largest
+// even number from the array of integers.
+
+
+// 9. Current Day Time - Write a JavaScript function that alerts the current day and time in the
+// following format:
+// Example:
+// Today is Friday.
+// It is 4:00PM.
+
+
+// 10. Unlimited Function - Write a JavaScript function that accepts an unlimited number of
+// arguments and prints them out in a single string in a single alert.
